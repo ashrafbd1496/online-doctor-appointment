ba0657b49c04851eceff4881c4364b8785f0114d
ashrafuddin1496 <asraf1496@gmail.com>
2021-01-05 10:33:12 +0600

    doctor view returned another way with abort

--- app/Http/Controllers/Doctor/Auth/RegisterController.php
+++ app/Http/Controllers/Doctor/Auth/RegisterController.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Controllers\Doctor\Auth;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+// use Illuminate\Support\Facades\View;
+
 
 class RegisterController extends Controller
 {
     public function register()
     {
-        return view('doctor.auth.register');
+        if(\View::exists('doctor.auth.register'))
+        {
+            return view('doctor.auth.register');
+        }
+
+        abort(403);
+
+        // abort(Response::HTTP_NOT_ACCEPTABLE);
+
     }
 }
